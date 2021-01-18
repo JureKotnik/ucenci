5c8eeec70f43149c17c9e5082c8c1c88e8a0fb1d
JureKotnik <jure2002k@gmail.com>
2021-01-18 12:27:33 +0100

    Insert kraji

--- ucenci/Addkraj.cs
+++ ucenci/Addkraj.cs
@@ -6,7 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms; 
+using System.Windows.Forms;
+using Npgsql;
 
 namespace ucenci
 {
@@ -22,6 +23,18 @@ namespace ucenci
             Form1 f1 = new Form1();
             f1.ShowDialog();
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox2.Text);
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=ec2-54-78-127-245.eu-west-1.compute.amazonaws.com;" + "Password=0f97f004987c14fa398b21069e1d5ecacc20742baa4c9265ad383d987721990e; Database=dbqabpjav305q8;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("INSERT INTO kraji (ime,posta) VALUES("+textBox1.Text+","+a+")", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                con.Close();
+            }
         }
     }
 }
