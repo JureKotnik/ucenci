d972176462caf057c9502c398e636bb98fcbc52d
JureKotnik <jure2002k@gmail.com>
2021-01-19 13:17:05 +0100

    kraji

--- ucenci/Brisanje_kraji.Designer.cs
+++ ucenci/Brisanje_kraji.Designer.cs
@@ -29,10 +29,17 @@ namespace ucenci
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Brisanje_kraji
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Brisanje_kraji";
+            this.Name = "Brisanje_kraji";
+            this.Text = "Kraji";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
