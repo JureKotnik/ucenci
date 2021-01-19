cd2dda6183ec1b3bdde39291ebed47c9286d0a94
JureKotnik <jure2002k@gmail.com>
2021-01-19 12:40:22 +0100

    brisanje_izbor_koda

--- ucenci/Brisanje_izbor.cs
+++ ucenci/Brisanje_izbor.cs
@@ -16,5 +16,48 @@ namespace ucenci
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String a = comboBox1.Text;
+
+            if (a == "Kraji")
+            {
+                Addkraj f4 = new Addkraj();
+                f4.ShowDialog();
+                this.Close();
+            }
+            if (a == "Dejavnosti")
+            {
+                AddDejavnost f5 = new AddDejavnost();
+                f5.ShowDialog();
+                this.Close();
+            }
+            if (a == "Dijaki")
+            {
+                Dijakics f4 = new Dijakics();
+                f4.ShowDialog();
+                this.Close();
+            }
+            if (a == "Naloge")
+            {
+                naloge f4 = new naloge();
+                f4.ShowDialog();
+                this.Close();
+            }
+            if (a == "Uporabniki")
+            {
+                Addkraj f4 = new Addkraj();
+                f4.ShowDialog();
+                this.Close();
+            }
+        }
     }
 }
