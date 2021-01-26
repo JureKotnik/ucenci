60998aba6ebd598f09b2d9fda27ff7849df52f2b
JureKotnik <jure2002k@gmail.com>
2021-01-26 12:44:11 +0100

    urejanje1

--- ucenci/Urejanje.Designer.cs
+++ ucenci/Urejanje.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ucenci
 {
-    partial class Brisanje_kraji
+    partial class Urejanje
     {
         /// <summary>
         /// Required designer variable.
@@ -29,31 +29,19 @@ namespace ucenci
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 71);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(713, 324);
-            this.listBox1.TabIndex = 7;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(216, 18);
+            this.button1.Location = new System.Drawing.Point(221, 18);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
+            this.button1.TabIndex = 9;
             this.button1.Text = "Prikaži";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // comboBox1
             // 
@@ -64,42 +52,30 @@ namespace ucenci
             "Kraji",
             "Naloge",
             "Uporabniki"});
-            this.comboBox1.Location = new System.Drawing.Point(65, 17);
+            this.comboBox1.Location = new System.Drawing.Point(68, 17);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 5;
+            this.comboBox1.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 24);
+            this.label1.Location = new System.Drawing.Point(12, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 17);
-            this.label1.TabIndex = 4;
+            this.label1.TabIndex = 7;
             this.label1.Text = "Prikaži";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(13, 415);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Izbriši";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
-            // 
-            // Brisanje_kraji
+            // Urejanje
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 512);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(877, 471);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
-            this.Name = "Brisanje_kraji";
-            this.Text = "Kraji";
+            this.Name = "Urejanje";
+            this.Text = "Urejanje";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,10 +83,8 @@ namespace ucenci
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
     }
 }
