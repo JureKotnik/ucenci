e02ac3d9d319ba5964f4284dc182da5a6de41cbc
JureKotnik <jure2002k@gmail.com>
2021-01-26 12:20:08 +0100

    dijaki.cs_odstranitev int a// brez uporabe

--- ucenci/Dodajanje/Dijakics.cs
+++ ucenci/Dodajanje/Dijakics.cs
@@ -27,7 +27,6 @@ namespace ucenci
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int a = Convert.ToInt32(textBox1.Text);
 
             using (NpgsqlConnection con = new NpgsqlConnection("Server=ec2-54-78-127-245.eu-west-1.compute.amazonaws.com;" + "Password=0f97f004987c14fa398b21069e1d5ecacc20742baa4c9265ad383d987721990e; Database=dbqabpjav305q8;"))
             {
