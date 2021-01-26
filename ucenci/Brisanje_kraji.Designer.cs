1ef6c972ba6d609ed7b61888a2da79cb6bd2e9f9
JureKotnik <jure2002k@gmail.com>
2021-01-26 11:48:54 +0100

    brisanje_kraji_konec

--- ucenci/Brisanje_kraji.Designer.cs
+++ ucenci/Brisanje_kraji.Designer.cs
@@ -31,6 +31,7 @@ namespace ucenci
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -52,11 +53,22 @@ namespace ucenci
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Prikaz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Brisanje_kraji
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(509, 369);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Brisanje_kraji";
@@ -69,5 +81,6 @@ namespace ucenci
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ucenci/Brisanje_kraji.cs
+++ ucenci/Brisanje_kraji.cs
@@ -18,10 +18,33 @@ namespace ucenci
             InitializeComponent();
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
             using (NpgsqlConnection con = new NpgsqlConnection("Server=ec2-54-78-127-245.eu-west-1.compute.amazonaws.com;" + "Password=0f97f004987c14fa398b21069e1d5ecacc20742baa4c9265ad383d987721990e; Database=dbqabpjav305q8;"))
             {
+
+                string a = listBox1.SelectedIndex.ToString();
+
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("DELETE GROM kraji WHERE ime="+a+";", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string ime = reader.GetString(2);
+                    listBox1.Items.Add(ime);
+                }
+                con.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=ec2-54-78-127-245.eu-west-1.compute.amazonaws.com;" + "Password=0f97f004987c14fa398b21069e1d5ecacc20742baa4c9265ad383d987721990e; Database=dbqabpjav305q8;"))
+            {
+
+
+
                 con.Open();
                 NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM kraji", con);
                 NpgsqlDataReader reader = com.ExecuteReader();
