24fbcb40de62e10f703f1857aa73015b7a27d1b3
JureKotnik <jure2002k@gmail.com>
2021-01-26 11:43:37 +0100

    Kraji select_brisanje

--- ucenci/Brisanje_kraji.cs
+++ ucenci/Brisanje_kraji.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace ucenci
 {
@@ -15,6 +16,22 @@ namespace ucenci
         public Brisanje_kraji()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=ec2-54-78-127-245.eu-west-1.compute.amazonaws.com;" + "Password=0f97f004987c14fa398b21069e1d5ecacc20742baa4c9265ad383d987721990e; Database=dbqabpjav305q8;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM kraji", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string ime = reader.GetString(2);
+                    listBox1.Items.Add(ime);
+                }
+                con.Close();
+            }
         }
     }
 }
